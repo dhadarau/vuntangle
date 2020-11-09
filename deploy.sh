d558516c0b501c5d967c93d790eaff2a348cba4d
dhadarau <dhadarau@untangle.com>
2020-11-09 19:25:22 +0200

    Travis

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:dhadarau/vuntangle.git master
+git push -f git@github.com:dhadarau/vuntangle.git master:gh-pages
 
 cd -
